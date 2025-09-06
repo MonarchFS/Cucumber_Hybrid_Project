c66b4d02d9381659408951d92bffdb5458be0143
MonarchFS <ronthegamer100@gmail.com>
2025-09-06 17:15:28 +0530

    Added ElementUtils methods and implemented in HomePage java class.

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -5,16 +5,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import utility.ElementUtils;
+
 public class HomePage {
 	WebDriver driver;
-	
+	private ElementUtils eleutils;
 	//constructor no return type
 	
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);//this line will automatically
 		//call all the methods of class homepage
-		
+		eleutils = new ElementUtils(driver);
+		//above we pass driver so that we can use it in eleutils object
 		
 	}
 	
@@ -39,25 +42,30 @@ public class HomePage {
 	//so above is private so cannot be called outside Homepoage class
 	//so below method will be used by other class to indirectly access above element
 	public void clickOnMyAccount() {
-		myAccountDropMenu.click();
+		//myAccountDropMenu.click();
+		eleutils.clickOnElement(myAccountDropMenu, 15);
 	}
 	
 	public LoginPage clickOnLogin() {
-		loginOption.click();
+		//loginOption.click();
+		eleutils.clickOnElement(loginOption, 20);
 		return new LoginPage(driver);
 	}
 	
 	public RegisterPage selectRegisterOption() {
-		registerOption.click();
+		//registerOption.click();
+		eleutils.clickOnElement(registerOption, 15);
 		return new RegisterPage(driver);
 	}
 	
 	public void enterProductIntoSearchBox(String ProductText) {
-		searchBoxField.sendKeys(ProductText);
+		//searchBoxField.sendKeys(ProductText);
+		eleutils.typeTextIntoElement(searchBoxField, ProductText, 20);
 	}
 	
 	public SearchResultPage clickOnSearchButton() {
-		searchButton.click();
+		//searchButton.click();
+		eleutils.clickOnElement(searchButton, 20);
 		return new SearchResultPage(driver);
 	}
 	
---
+++ src/main/java/utility/ElementUtils.java
@@ -0,0 +1,135 @@
+package utility;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ElementUtils {
+	
+	//constructor to get the driver
+	WebDriver driver;
+	public ElementUtils(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	//click on Element method
+	
+	public void clickOnElement(WebElement element, long durationInSeconds) {
+		
+		//Wait for element to be clickable
+		//WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(durationInSeconds));
+		WebElement webElement= waitForElement(element, durationInSeconds);
+		webElement.click();
+	}
+	
+	//type text element
+	
+	public void typeTextIntoElement(WebElement element, String textToBeTyped, long durationInSeconds) {
+		//WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(durationInSeconds));
+		
+		WebElement webElement= waitForElement(element, durationInSeconds);
+		webElement.click();
+		webElement.clear();
+		webElement.sendKeys(textToBeTyped);
+		
+		
+	}
+	
+	public WebElement waitForElement(WebElement element, long durationInSeconds) {
+		WebElement webElement = null;;
+		
+		try {	
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(durationInSeconds));
+		webElement= wait.until(ExpectedConditions.elementToBeClickable(element));
+		}
+		catch(Throwable e) {
+			e.printStackTrace();
+		}
+		return webElement;
+	}
+	
+	public void selectOptionInDropdown(WebElement element,String dropDownOption, long durationInSeconds) {
+		WebElement webelement = waitForElement(element, durationInSeconds);
+		Select select = new Select(element);
+		select.deselectByVisibleText(dropDownOption);
+		
+	}
+	
+	public void acceptAlert(long durationOfSeconds) {
+		Alert alert = waitForAlert(durationOfSeconds);
+		alert.accept();
+	}
+	
+	public void dismissAlert(long durationOfSeconds) {
+		Alert alert = waitForAlert(durationOfSeconds);
+		alert.dismiss();
+	}
+	
+	public Alert waitForAlert(long durationInSeconds) {
+		
+		Alert alert = null;
+		try {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(durationInSeconds));
+		alert =  wait.until(ExpectedConditions.alertIsPresent());
+		}
+		catch(Throwable e){
+			e.printStackTrace();
+		}
+		return alert;
+	}
+	
+	public void mouseHoverAndClick(WebElement element,long durationInSeconds) {
+		
+		//WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(durationInSeconds));
+		WebElement webElement = waitForVisibilityOfElement(element, durationInSeconds);
+		
+		Actions action = new Actions(driver);
+		action.moveToElement(webElement).click().build().perform();
+	}
+	
+	//to separate waits for different actions
+	public WebElement waitForVisibilityOfElement(WebElement element,long durationInSeconds) {
+		
+		WebElement webElement = null;  // if null not given then issue as its not initialized yet
+		
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(durationInSeconds));
+			webElement = wait.until(ExpectedConditions.visibilityOf(element));
+		} catch (Throwable e) {
+			
+			e.printStackTrace();
+		}
+		
+		return webElement;
+	}
+	
+	
+	//javascript click method if normal click donot work
+	
+	public void javascriptClick(WebElement element,long durationOfSeconds) {
+		
+		WebElement webElement = waitForVisibilityOfElement(element, durationOfSeconds);
+		//typecast driver to use javascript
+		
+		JavascriptExecutor jse= ((JavascriptExecutor)driver );
+		jse.executeScript("argument[0].click();", webElement);
+		
+	}
+	
+	//javascript sendkey type method if normal sendkeys donot work
+	public void javascriptType(WebElement element,long durationOfSeconds,String textToBeTyped) {
+		WebElement webElement = waitForVisibilityOfElement(element, durationOfSeconds);
+		JavascriptExecutor jse = ((JavascriptExecutor)driver);
+		jse.executeScript("arguments[0].value='"+textToBeTyped+"'", webElement);
+		
+	}
+	
+	
+}
